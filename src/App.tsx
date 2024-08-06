db00c86381ec6130d8e11bf1f4790898047f5564
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-08-06 17:38:25 +0530

    Minor formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,55 @@
-import { useEffect, useState } from 'react'
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
-import './App.css'
-import WelcomeScreen from './screens/auth/welcome';
-import ChooseAuthScreen from './screens/auth/auth';
-import GenerateSeedPhrase from './screens/auth/create_wallet/get_seed';
-import SetWalletPassword from './screens/auth/create_wallet/set_password';
-import HomeScreen from './screens/home';
-import { Toaster } from 'react-hot-toast';
-import LoginScreen from './screens/ask-login';
-import GetWalletFromSeed from './screens/auth/import_wallet/get-wallet-seed';
-import { useAtom } from 'jotai';
-import { isLoggedInAtom } from './atom/global';
-import BottomBar from './components/bottom-bar';
+import "./App.css";
+import WelcomeScreen from "./screens/auth/welcome";
+import ChooseAuthScreen from "./screens/auth/auth";
+import GenerateSeedPhrase from "./screens/auth/create_wallet/get_seed";
+import SetWalletPassword from "./screens/auth/create_wallet/set_password";
+import HomeScreen from "./screens/home";
+import { Toaster } from "react-hot-toast";
+import LoginScreen from "./screens/ask-login";
+import GetWalletFromSeed from "./screens/auth/import_wallet/get-wallet-seed";
+import { useAtom } from "jotai";
+import { isLoggedInAtom } from "./atom/global";
+import BottomBar from "./components/bottom-bar";
 
 function App() {
-
   const navigate = useNavigate();
   const location = useLocation();
-  
-  const [isLoggedin, setIsLoggedin] = useAtom(isLoggedInAtom); 
 
-  useEffect(()=> {
+  const [isLoggedin, setIsLoggedin] = useAtom(isLoggedInAtom);
 
+  useEffect(() => {
     let encWallet = localStorage.getItem("wallet");
 
     if (encWallet === null) {
-      navigate("/")
+      navigate("/");
     } else {
-      navigate("/login")
+      navigate("/login");
       setIsLoggedin(true);
     }
-    console.log(location)
-  },[])
-
+    console.log(location);
+  }, []);
 
   return (
     <div className="w-screen min-h-screen bg-black flex flex-col">
-        <Routes>
-          <Route path="/" element={<WelcomeScreen />} />
-          <Route path="/auth" element={<ChooseAuthScreen />} />
-          <Route path="/auth/create-wallet" element={<GenerateSeedPhrase />} />
-          <Route path="/auth/get-wallet-seed" element={<GetWalletFromSeed />} />
-          <Route path="/auth/set-wallet-password" element={<SetWalletPassword />} />
-          <Route path="/home" element={<HomeScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-
-        </Routes>
-        {
-          isLoggedin && location.pathname !== "/login" && <BottomBar/>
-        }
-        <Toaster/>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/auth" element={<ChooseAuthScreen />} />
+        <Route path="/auth/create-wallet" element={<GenerateSeedPhrase />} />
+        <Route path="/auth/get-wallet-seed" element={<GetWalletFromSeed />} />
+        <Route
+          path="/auth/set-wallet-password"
+          element={<SetWalletPassword />}
+        />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+      {isLoggedin && location.pathname !== "/login" && <BottomBar />}
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,21 +1,18 @@
 import { useNavigate } from "react-router-dom";
 
 export default function HomeScreen() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    return (
-        <>
-
-            <div className="px-10 mt-10 flex-1 flex flex-col justify-between">
-                <div>
-                    <p className="text-2xl font-bold text-white">Welcome to CoinYoink!</p>
-                    <p className="mt-3 text-gray-400 text-[14px]">Ready to Yoink Some Coins? Let's Get You Started! 🪙🚀</p>
-                </div>
-            </div>
-
-
-
-        </>
-    );
+  return (
+    <>
+      <div className="px-10 mt-10 flex-1 flex flex-col justify-between">
+        <div>
+          <p className="text-2xl font-bold text-white">Welcome to CoinYoink!</p>
+          <p className="mt-3 text-gray-400 text-[14px]">
+            Ready to Yoink Some Coins? Let's Get You Started! 🪙🚀
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
