f200027de1663ed92206be3dc2b5837dddb07942
Swapnil Shinde <swapnilshinde9382@gmail.com>
2024-11-16 21:43:11 +0700

    web3 provider infura

---
+++ src/providers/Web3Provider.tsx
@@ -0,0 +1,7 @@
+import { ethers } from "ethers";
+import Web3 from "web3";
+
+const INFURA = `https://sepolia.infura.io/v3/${import.meta.env.VITE_INFURA_API_KEY}`;
+export const web3: Web3 = new Web3(new Web3.providers.HttpProvider(INFURA));
+
+export const ethersProvider = new ethers.JsonRpcProvider(`https://sepolia.infura.io/v3/${import.meta.env.VITE_INFURA_API_KEY}`);
