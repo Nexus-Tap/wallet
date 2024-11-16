2f5066ac44cf7b9fccbaa53726ff3dd0d0628d5e
Swapnil Shinde <swapnilshinde9382@gmail.com>
2024-11-16 21:41:54 +0700

    global config added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import fs from 'fs';
 
 export default defineConfig({
-  plugins: [react()],
+  logLevel: 'info',
+  plugins: [
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: '0.0.0.0', // Optional: Can specify '0.0.0.0' to access from any device on the network
+    port: 3000, // Optional: You can choose a different port if needed
+  },
+  define: {
+    'global': {},
   },
 });
